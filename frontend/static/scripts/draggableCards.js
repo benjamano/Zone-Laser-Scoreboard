9fd254d5e0da1c726d67587a89434a2b6757d741
Ben Mercer <benmercer76@btinternet.com>
2025-02-21 23:34:54 +0000

    Added some more draggable card options

--- frontend/static/scripts/draggableCards.js
+++ frontend/static/scripts/draggableCards.js
@@ -1,12 +1,42 @@
-function addCard() {
+function addSmallCard() {
     const container = document.querySelector(".movableItemsContainer");
+
+    card = createCard();
+
+    card.classList.add("smallCard");
+
+    container.appendChild(card);
+}
+
+function addLargeCard() {
+    const container = document.querySelector(".movableItemsContainer");
+
+    card = createCard();
+
+    card.classList.add("largeCard");
+
+    container.appendChild(card);
+}
+
+function addWideCard() {
+    const container = document.querySelector(".movableItemsContainer");
+
+    card = createCard();
+
+    card.classList.add("wideCard");
+
+    container.appendChild(card);
+}
+
+function createCard(){
+    const container = document.querySelector(".movableItemsContainer");
+    
     const card = document.createElement("div");
     card.classList.add("draggableCard");
     card.textContent = "Drag Me";
     card.style.left = "0px";
     card.style.top = "0px";
     card.draggable = true;
-    container.appendChild(card);
 
     card.addEventListener("dragstart", (e) => {
         e.dataTransfer.setData("text/plain", JSON.stringify({
@@ -19,9 +49,11 @@ function addCard() {
     card.addEventListener("dragend", (e) => {
         card.classList.remove("dragging");
         let containerRect = container.getBoundingClientRect();
-        let x = Math.round((e.pageX - containerRect.left) / 100) * 100;
-        let y = Math.round((e.pageY - containerRect.top) / 100) * 100;
+        let x = Math.round((e.pageX - containerRect.left - card.offsetWidth / 2) / 100) * 100;
+        let y = Math.round((e.pageY - containerRect.top - card.offsetHeight / 2) / 100) * 100;
         card.style.left = `${x}px`;
         card.style.top = `${y}px`;
     });
+
+    return card;
 }
