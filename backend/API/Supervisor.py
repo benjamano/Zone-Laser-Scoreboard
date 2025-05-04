8dd98a81abc2bc29377f7f031a2f62be36b515fd
Ben Mercer <benmercer76@btinternet.com>
2025-05-05 00:02:29 +0100

    Hotfix for time usage in sleep mode

--- backend/API/Supervisor.py
+++ backend/API/Supervisor.py
@@ -157,7 +157,7 @@ class Supervisor:
                                 startTime = datetime.fromisoformat(foundGame.startTime) if isinstance(foundGame.startTime, str) else foundGame.startTime
                                 timeToCheck = datetime.now() + timedelta(minutes=-30)
                                 currentTime = datetime.now().time()
-                                if startTime < timeToCheck and self._obs != None and (currentTime < time(11, 0) or currentTime > time(17, 0)):
+                                if startTime < timeToCheck and self._obs != None and (currentTime < time.time(11, 0) or currentTime > time.time(17, 0)):
                                     # message(f"Found game with end time: {foundGame.endTime}, time to check is {timeToCheck} setting OBS output to sleep mode.")
                                     self._obs.switchScene("Test Mode")
                                 
