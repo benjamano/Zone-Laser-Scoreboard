eb3ee86a592b0926a24d04f970b1545cb914c6ef
Ben Mercer <benmercer76@btinternet.com>
2025-05-22 17:34:54 +0100

    Bug Fix - Playing the briefing no longer freezes the web app

--- backend/webApp.py
+++ backend/webApp.py
@@ -1216,13 +1216,11 @@ class WebApp:
         @self.socketio.on('playBriefing')
         def playBriefing():
             if self._obs != None and self._obs.isConnected() == True:
-                try:
-                    f.message("Playing briefing")
-                    
+                try:                    
                     #f.message("Playing briefing")
                     self._obs.switchScene("Video")
                     
-                    return 200
+                    return jsonify({"status": "success"}), 200
                 except Exception as e:
                     ise : InternalServerError = InternalServerError()
                 
@@ -1235,7 +1233,7 @@ class WebApp:
 
             else:
                 #f.message("OBS not connected, cannot play breifing!", type="warning")
-                return 500
+                return jsonify({"status": "error", "message": "OBS not connected, cannot play briefing!"}), 500
     
         @self.app.route('/sendMessage', methods=['POST'])
         def sendMessage():
