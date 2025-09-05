9d6cfe1ec8539c5453f2cdc9b1606f7ec94dd8a3
Ben Mercer <benmercer76@btinternet.com>
2025-09-05 18:33:31 +0100

    Activity checker bug fix

--- backend/activityChecker.py
+++ backend/activityChecker.py
@@ -45,7 +45,7 @@ while True:
         else:
             tries += 1
             if tries == 3:
-                subprocess.Popen(["python", fr"{dir}\runPython.bat"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+                subprocess.Popen([fr"{dir}\runPython.bat"], creationflags=subprocess.CREATE_NEW_CONSOLE)
             elif tries > 12:
                 os.system("shutdown /r /t 1")
                 tries = 0
